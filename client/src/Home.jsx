f0fab21084016cdfd725bd935f78fa2e654c9c49
rola <rolaalotabe@gmail.com>
2024-07-04 08:10:13 +0300

    data

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -6,16 +6,37 @@ import { useEffect } from "react";
 
 const Home = () => {
   const [showCreate, setShowCreate] = useState(false);
-  const [posts, setPosts] = useState(null);
+  const [posts, setPosts] = useState([
+    {
+      userName: "John Doe",
+      title: "My first blog post",
+      date: "2021-08-01",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.",
+      initialLike: 0,
+      views: 100,
+    },
+    {
+      userName: "BOB",
+      title: "My first blog post",
+      date: "2021-08-01",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.",
+      initialLike: 0,
+      views: 100,
+    },
+    
+   
+  ]);
 
-useEffect(() => {
-fetch("https://group-two.midend.tech/api/blogLike").then((res) => {
-  return res.json();})
-  .then((data) => {
-   console.log(data);
-});
+// useEffect(() => {
+// fetch("https://group-two.midend.tech/api/blogLike").then((res) => {
+//   return res.json();})
+//   .then((data) => {
+//    console.log(data);
+// });
   
-  }, []);
+  // }, []);
 
   const addPost = (newPost) => {
     // const date = new Date().toISOString().split("T")[0];
